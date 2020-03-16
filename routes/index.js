9f2b01a62d8c2ad5ea0f652e57b74c4df58bb1e7
fayyaz <fayyaz@codezilla.io>
2020-03-16 08:09:18 +0000

    new change

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var sumController = require("../controllers/sum-controller")
 /* GET home page. */
 router.get('/', function (req, res, next) {
   let sum = sumController.sum(2, 5);
-  res.render('index', { title: 'Hello Codezilla Pvt Ltd ', 'sum': sum });
+  res.render('index', { title: 'Hello Codezilla Pvt Ltd', 'sum': sum });
 });
 
 module.exports = router;
